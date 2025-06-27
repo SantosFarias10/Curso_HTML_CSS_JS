0ce9e364c7ec702417c6163ba0b820ec26828fe8
SantosFarias10 <santos.farias@mi.unc.edu.ar>
2025-06-27 10:59:14 -0300

    {ADD} DOM

---
+++ JS/DOM/main.js
@@ -0,0 +1,7 @@
+window.onload = () => { // Esperamos que termine de cargar toda la pagina
+  const parrafo = document.getElementById("text")
+  //console.log(parrafo.innerText)
+  //parrafo.innerText = "New Text"
+  //console.log(parrafo.innerText)
+  //parrafo.innerHTML = '<ul><li>Elemento 1</li><li>Elemento 2</li></ul>'
+}
